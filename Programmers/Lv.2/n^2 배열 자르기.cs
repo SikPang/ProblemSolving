8198cba924e71928d1ca6477f00c59830cbe93bf
SikPang <87380790+SikPang@users.noreply.github.com>
2022-08-21 12:38:45 +0900

    Day 42

--- Programmers/Lv.2/n^2 배열 자르기.cs
+++ Programmers/Lv.2/n^2 배열 자르기.cs
@@ -4,15 +4,10 @@ public class Solution
 {
     public int[] solution(int n, long left, long right)
     {
-        int[] answer = new int[right-left+1];
+        int[] answer = new int[right - left + 1];
 
-        for(long i=left; i<right+1; i++)
-        {
-            if (i == 0)
-                answer[i - left] = 1;
-            else
-                answer[i - left] = (int)Math.Max(i / n, i % n) + 1;
-        }
+        for (long i = left; i < right + 1; i++)
+            answer[i - left] = (int)Math.Max(i / n, i % n) + 1;
 
         return answer;
     }
