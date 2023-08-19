0069f804a0a89e866e77e0cf56e01e01246a8304
Kwangsik Song <kwsong@c4r4s1.42seoul.kr>
2023-08-19 18:00:31 +0900

    Day 73

---
+++ Programmers/Lv.2/시소 짝꿍.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+unsigned long long solution(vector<int> weights) {
+    unsigned long long answer = 0;
+	unordered_map<int, unsigned long long> counter;
+	
+	for (int i = 0; i < weights.size(); ++i)
+	{
+		int target = weights[i];
+		if (counter.find(target) != counter.end())
+			++counter[target];
+		else
+			counter.insert(make_pair(target, 1));
+	}
+
+	for (unordered_map<int, unsigned long long>::iterator iter = counter.begin(); iter != counter.end(); ++iter)
+	{
+		answer += iter->second * (iter->second - 1) / 2;
+		if (counter.find(iter->first * 2) != counter.end())
+			answer += counter[iter->first * 2];
+		if (counter.find(iter->first * 3.0 / 2.0) != counter.end())
+			answer += counter[iter->first * 3.0 / 2.0];
+		if (counter.find(iter->first * 4.0 / 3.0) != counter.end())
+			answer += counter[iter->first * 4.0 / 3.0];
+	}
+    return answer;
+}
+
+// nCr = nPr / r!
+// nPr = n! / (n - r)!
+
+// nC2 = nP2 / 2!
+// nP2 = n! / (n - 2)!
+
+// n!		= n * (n - 1) * (n - 2) * (n - 3) ...
+// (n - 2)!	= 				(n - 2) * (n - 3) ...
+// nP2		= n * (n - 1)
+// 2! = 2
+
+// nC2 = n * (n - 1) / 2
+
+// 몸무게를 key로 가지고, 인원을 value로 가지는 counter 생성
+// 현재 몸무게를 *2, *3/2, *4/3 연산한 몸무게를 counter에서 찾음
+// 현재 순회 몸무게가 3명이고 counter에서 찾은 몸무게가 2명이 있다면 짝꿍은 6쌍 나오므로 answer += 6
+// 본인과 같은 몸무게 인원이 짝을 이룰 수 있는 경우의 수 
