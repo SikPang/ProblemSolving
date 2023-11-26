82ddd5c862fffbbe27edf6960a2d3b018598edd6
SikPang <songks91@naver.com>
2023-11-26 23:13:15 +0900

    merge

---
+++ Algorithm/MergeSort/MergeSort.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <vector>
+
+namespace merge
+{
+void sort(std::vector<int>& buf);
+}
---
+++ Algorithm/MergeSort/main.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include "MergeSort.hpp"
+
+static void sortChecker(std::vector<int>& buf)
+{
+	for (int i=0; i<(int)buf.size()-1; ++i)
+	{
+		if (buf[i] > buf[i+1])
+		{
+			std::cout << "\e[31mX\e[0m ";
+			return;
+		}
+	}
+	std::cout << "\e[32mV\e[0m ";
+}
+
+int main(int ac, char** av)
+{
+	if (ac < 2)
+	{
+		std::cout << "<Usage> : ./a.out 5 2 4 1 3\n";
+		return 1;
+	}
+
+	std::vector<int> buf;
+	buf.reserve(ac);
+
+	for (int i=1; i<ac; ++i)
+		buf.push_back(atoi(av[i]));
+
+	merge::sort(buf);
+	
+	sortChecker(buf);
+	return 0;
+}
