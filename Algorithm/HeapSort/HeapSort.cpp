76aec16e8fbe0ad24e4cb99a259161c87791348e
SikPang <songks91@naver.com>
2023-11-30 20:25:55 +0900

    a

--- Algorithm/HeapSort/HeapSort.cpp
+++ Algorithm/HeapSort/HeapSort.cpp
@@ -1,10 +1,42 @@
-#include "heapSort.hpp"
+#include "HeapSort.hpp"
 
 namespace heap
 {
 
+// 최대 힙
 void sort(std::vector<int>& buf)
 {
+	// 이미 돼있다.
+
+	/*
+					0
+			1				2
+		3		4		5		6
+	7	8	9
+	
+	2x+1, 2x+2
+
+	(x-1)/2
+	
+	0, 1, 2
+	1, 3, 4
+	2, 5, 6
+	3, 7, 8
+	4,  9, 10
+	*/
+
+	
+}
 
 }
+
+#include <vector>
+#include <iostream>
+int main()
+{
+	std::vector<int> buf = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	heap::sort(buf);
+	for (int i=0; i<buf.size(); ++i)
+		std::cout << buf[i] << " ";
+	std::cout << "\n";
 }
