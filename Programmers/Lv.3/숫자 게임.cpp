d759d76d20ea615a362afd18e00cc179116a9113
kwang-sik Song <songks91@naver.com>
2024-01-19 08:39:57 +0900

    Day 112

---
+++ Programmers/Lv.3/숫자 게임.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+    int answer = 0;
+    
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    
+    for (int i=0; i<A.size(); ++i)
+    {
+        if (A[answer] < B[i])
+            ++answer;
+    }
+    return answer;
+}
